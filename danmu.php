b1dab1625fe43eeae3d1b3aad039155727be600f
wjh <wjhokey@gmail.com>
2018-11-18 19:25:23 +0800

    增加多进程、swoole版本

--- danmu.php
+++ danmu.php
@@ -3,37 +3,41 @@
 class Danmu
 {
 
-    public function __construct()
-    {
+    /**
+     * 默认房间id
+     * @var int
+     */
+    public $roomId = 288016;
 
+    public $sock;
+
+    public function __construct($roomId)
+    {
+        $this->roomId = $roomId;
+
+        $ip = gethostbyname("openbarrage.douyutv.com");
+        $port = 8601;
+
+        if(($this->sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP)) < 0) {
+            echo "socket_create() 失败的原因是:".socket_strerror($this->sock)."\n";
+        }
+
+        if (($result = socket_connect($this->sock, $ip, $port)) < 0){
+            echo "socket_connect() 失败的原因是:".socket_strerror($result) . "\n";
+        }
     }
 
     public function handle()
     {
-        $ip = gethostbyname("openbarrage.douyutv.com");
-        $port = 8601;
-
-        if(($sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP)) < 0) {
-            echo "socket_create() 失败的原因是:".socket_strerror($sock)."\n";
-        }
-
-        if (($result = socket_connect($sock, $ip, $port)) < 0){
-            echo "socket_connect() 失败的原因是:".socket_strerror($result) . "\n";
-        }
-
         //登录
-        $msg = "type@=loginreq/roomid@=288016/\0";
-        $packMsg = $this->packMsg($msg);
-        socket_write($sock, $packMsg, strlen($packMsg));
+        $this->login();
 
         //获取弹幕请求
-        $joinMsg = 'type@=joingroup/rid@=288016/gid@=-9999/\0';
-        $packJoinMsg = $this->packMsg($joinMsg);
-        socket_write($sock, $packJoinMsg, strlen($packJoinMsg));
+        $this->joinRoom();
 
         $time = time();
 
-        while($content = socket_read($sock, 1024)){
+        while($content = socket_read($this->sock, 1024)){
             preg_match('/nn@=(.*?)\//', $content, $name);
             preg_match('/txt@=(.*?)\/cid/', $content, $text);
             if (empty($name)) continue;
@@ -41,26 +45,44 @@ class Danmu
             $text = $text[1]??'';
             echo date("Y-m-d H:i:s"). ' ['. $name .']: '. $text . "\n";
 
-
-            if (time()-$time > 20) {
-                $this->keepLive($sock);
+            //发送心跳包
+            if (time() - $time > 20) {
+                $this->keepLive();
                 $time = time();
             }
 
         }
 
-        socket_close($sock);
+        socket_close($this->sock);
     }
 
+    private function login()
+    {
+        $msg = "type@=loginreq/roomid@=".$this->roomId. "/\0";
+        $packMsg = $this->packMsg($msg);
+        socket_write($this->sock, $packMsg, strlen($packMsg));
+    }
 
-    private function keepLive($sock)
+    private function keepLive()
     {
         $keepMsg = 'type@=keeplive/tick@='. time(). '/\0';
         $packKeepMsg = $this->packMsg($keepMsg);
-        socket_write($sock, $packKeepMsg, strlen($packKeepMsg));
+        socket_write($this->sock, $packKeepMsg, strlen($packKeepMsg));
         echo date("Y-m-d H:i:s"). " 发送心跳包\n";
     }
 
+    private function joinRoom()
+    {
+        $joinMsg = 'type@=joingroup/rid@=288016/gid@=-9999/\0';
+        $packJoinMsg = $this->packMsg($joinMsg);
+        socket_write($this->sock, $packJoinMsg, strlen($packJoinMsg));
+    }
+
+    /**
+     * 打包数据
+     * @param $str
+     * @return string
+     */
     private function packMsg($str){
         $length = pack('V', 4 + 4 + strlen($str) + 1);
         $code = $length;
@@ -72,5 +94,9 @@ class Danmu
 
 }
 
-$danmu = new Danmu();
-$danmu->handle();
+$roomId = 288016;
+$danmu = new Danmu($roomId);
+//$danmu->handle();
+
+
+new swoole_http_server();

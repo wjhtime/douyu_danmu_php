6d971f7c0115a6e98ecc9de3fbb613a3b301e1a6
wjh <wjhokey@gmail.com>
2018-11-20 13:07:25 +0800

    多进程实现+代码优化

--- danmu_pcntl.php
+++ danmu_pcntl.php
@@ -1,72 +1,42 @@
 <?php
 
-class Danmu
+class Sock
 {
-
-    /**
-     * 默认房间id
-     * @var int
-     */
-    public $roomId = 288016;
-
     public $sock;
+    static $instance;
 
-    public function __construct($roomId)
+    // 登录
+    const LOGIN = 'login';
+    // 保持心跳
+    const KEEP_LIVE = 'keep_live';
+    //加入房间
+    const JOIN_ROOM = 'join_room';
+    //登出
+    const LOGOUT = 'logout';
+
+    public $msg = [
+        self::LOGIN => "type@=loginreq/roomid@=%s/\0",
+        self::KEEP_LIVE => 'type@=mrkl/\0',
+        self::JOIN_ROOM => 'type@=joingroup/rid@=%d/gid@=-9999/\0',
+        self::LOGOUT => 'type@=logout/\0',
+    ];
+
+    public function __construct($sock)
     {
-        $this->roomId = $roomId;
+        $this->sock = $sock;
+    }
 
-        $ip = gethostbyname("openbarrage.douyutv.com");
-        $port = 8601;
-
-        if(($this->sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP)) < 0) {
-            echo "socket_create() 失败的原因是:".socket_strerror($this->sock)."\n";
+    public static function instance()
+    {
+        if (!self::$instance) {
+            $ip = gethostbyname("openbarrage.douyutv.com");
+            $port = 8601;
+            $sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
+            socket_connect($sock, $ip, $port);
+            self::$instance = new self($sock);
         }
 
-        if (($result = socket_connect($this->sock, $ip, $port)) < 0){
-            echo "socket_connect() 失败的原因是:".socket_strerror($result) . "\n";
-        }
-    }
-
-    public function handle()
-    {
-        //登录
-        $this->login();
-
-        //获取弹幕请求
-        $this->joinRoom();
-
-        while($content = socket_read($this->sock, 1024)){
-            preg_match('/nn@=(.*?)\//', $content, $name);
-            preg_match('/txt@=(.*?)\/cid/', $content, $text);
-            if (empty($name)) continue;
-            $name = $name[1]??'';
-            $text = $text[1]??'';
-            echo date("Y-m-d H:i:s"). ' ['. $name .']: '. $text . "\n";
-        }
-
-        socket_close($this->sock);
-    }
-
-    private function login()
-    {
-        $msg = "type@=loginreq/roomid@=".$this->roomId. "/\0";
-        $packMsg = $this->packMsg($msg);
-        socket_write($this->sock, $packMsg, strlen($packMsg));
-    }
-
-    public function keepLive()
-    {
-        $keepMsg = 'type@=keeplive/tick@='. time(). '/\0';
-        $packKeepMsg = $this->packMsg($keepMsg);
-        socket_write($this->sock, $packKeepMsg, strlen($packKeepMsg));
-        echo date("Y-m-d H:i:s"). " 发送心跳包\n";
-    }
-
-    private function joinRoom()
-    {
-        $joinMsg = 'type@=joingroup/rid@=288016/gid@=-9999/\0';
-        $packJoinMsg = $this->packMsg($joinMsg);
-        socket_write($this->sock, $packJoinMsg, strlen($packJoinMsg));
+        return self::$instance;
     }
 
     /**
@@ -82,26 +52,76 @@ class Danmu
         return $length.$code.$magic.$str.$end;
     }
 
+    /**
+     * 发送消息
+     * @param $msg
+     */
+    public function sendMsg($msg)
+    {
+        $packMsg = $this->packMsg($msg);
+        socket_write($this->sock, $packMsg, strlen($packMsg));
+    }
+
+    /**
+     * 读取消息
+     * @return string
+     */
+    public function read()
+    {
+        return socket_read($this->sock, 1024);
+    }
+
+    /**
+     * 关闭连接
+     */
+    public function close()
+    {
+        return socket_close($this->sock);
+    }
 
 }
 
 
+
+$sock = Sock::instance();
+
+pcntl_signal(SIGTERM, function () use ($sock) {
+    $sock->sendMsg($sock->msg[Sock::LOGOUT]);
+});
+
+
 $pid = pcntl_fork();
-$roomId = 288016;
 //主进程
 if ($pid) {
-    $danmu = new Danmu($roomId);
-    $danmu->handle();
-} else {
-    $danmu = new Danmu($roomId);
-    //子进程
+    $roomId = 288016;
+    $sock->sendMsg(sprintf($sock->msg[Sock::LOGIN], $roomId));
+    $sock->sendMsg(sprintf($sock->msg[Sock::JOIN_ROOM], $roomId));
+
+    while ($content = $sock->read()) {
+        preg_match('/nn@=(.*?)\//', $content, $name);
+        preg_match('/txt@=(.*?)\/cid/', $content, $text);
+        if (empty($name)) continue;
+        $name = $name[1]??'';
+        $text = $text[1]??'';
+        echo date("Y-m-d H:i:s"). ' ['. $name .']: '. $text . "\n";
+    }
+}
+else
+//子进程
+{
     $time = time();
     //发送心跳包
     while (true) {
         if (time() - $time > 20) {
-            $danmu->keepLive();
+            $sock->sendMsg($sock->msg[Sock::KEEP_LIVE]);
             $time = time();
+            echo date("Y-m-d H:i:s"). " 发送心跳包 \n";
         }
     }
 }
+
+
+
+
+
 
